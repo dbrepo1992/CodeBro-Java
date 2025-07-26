17c27446d24d5916d92cd00c393fe689cfcfa9d5
Damian Bugaj <damianbugajit@gmail.com>
2025-07-26 20:08:47 +0200

    Add much more examples for methods

--- SecondPart/methods/src/Main.java
+++ SecondPart/methods/src/Main.java
@@ -92,5 +92,48 @@ public class Main {
         }
 
          */
+
+        // ---------------- MORE EXAMPLES WITH METHODS
+
+
+    // double result = square(3);
+    //    System.out.println(result);
+    //    System.out.println(square(3)); // print exactly the same
+
+        /*
+
+        System.out.println(cube(3));
+
+        String fullName = getFullName("Spongebob", "Squarepants");
+        System.out.println(fullName);
+
+        int age = 11;
+        if (ageCheck(age)){
+            System.out.println("You may sign up!");
+        } else {
+            System.out.println("You must be 18 to sign up!");
+        }
+
+    }
+    static double square(double number){
+        return  number * number;
+    }
+
+    static double cube(double number){
+       return number * number * number;
+    }
+
+    static String getFullName(String first, String last){
+        return first + " " + last;
+    }
+
+    static boolean ageCheck(int age){
+        if (age >= 18){
+            return  true;
+        } else {
+            return  false;
+        }
     }
 }
+
+         */
