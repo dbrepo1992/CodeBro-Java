b6c86d1d1c23554bd42a0ae80a0ca265559185a4
Damian Bugaj <damianbugajit@gmail.com>
2025-08-01 20:49:34 +0200

    Working on Quiz game

--- SecondPart/Quiz game/src/Main.java
+++ SecondPart/Quiz game/src/Main.java
@@ -6,23 +6,16 @@ public class Main {
         Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
 
         String[] questions = {"Which of the following is a compiled programming language?",
-                "In Python, what does the function len() do?"
-                , "Which symbol is used for single-line comments in C++?"};
+                              "In Python, what does the function len() do?",
+                              "Which symbol is used for single-line comments in C++?"};
 
-        String[][] answersGroup = {{"1) Python, " +
-                "2) Java, " +
-                "3) JavaScript, " +
-                "4) HTML"},
-                {"1) Returns the length of an object, " +
-                        "2) Converts a number to a string, " +
-                        "3) Creates a list, " +
-                        "4) Returns the last element of a list"},
-                {"1) #, " +
-                        "2) //, " +
-                        "3) <!-- -->, " +
-                        "4) /* */"}};
+        String[][] options = {{"1) Python", "2) Java", "3) JavaScript", "4) HTML"},
+                              {"1) Returns the length of an object", "2) Converts a number to a string","3) Creates a list", "4) Returns the last element of a list"},
+                              {"1) #", "2) //", "3) <!-- -->", "4) /* */"}};
 
-        int choice;
+        int[] answers = {2, 1, 2};
+        int score = 0;
+        int guess;
 
         System.out.println("******************************");
         System.out.println("Welcome to the Java Quiz Game!");
