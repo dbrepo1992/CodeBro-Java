7fff84d4b143203998bec44720f08e52e7cbb0d2
Damian Bugaj <damianbugajit@gmail.com>
2025-08-02 10:41:23 +0200

    Finished quiz game

--- SecondPart/Quiz game/src/Main.java
+++ SecondPart/Quiz game/src/Main.java
@@ -13,22 +13,36 @@ public class Main {
                               {"1) Returns the length of an object", "2) Converts a number to a string","3) Creates a list", "4) Returns the last element of a list"},
                               {"1) #", "2) //", "3) <!-- -->", "4) /* */"}};
 
+
+
         int[] answers = {2, 1, 2};
         int score = 0;
-        int guess;
+        int guess = 0;
 
         System.out.println("******************************");
         System.out.println("Welcome to the Java Quiz Game!");
         System.out.println("*******************************");
 
-
-
-            for (int i = 0; i < questions.length; i++){
-                System.out.println(questions[i]);
+        for (int i = 0; i < questions.length; i++){
+            System.out.println(questions[i]);
+            for (String option : options[i]){
+                System.out.println(option);
             }
+            System.out.print("Enter your guess:");
+            guess = scanner.nextInt();
+            if (guess == answers[i]) {
+                System.out.println("**************");
+                System.out.println("You are right!");
+                System.out.println("**************");
+                score++;
+            } else {
+                System.out.println("**************");
+                System.out.println("You are wrong!");
+                System.out.println("**************");
+            }
+        }
 
-        System.out.println(answersGroup.length);
-
+        System.out.println("Your score is: " + score + " out of " + questions.length);
 
         scanner.close();
     }
